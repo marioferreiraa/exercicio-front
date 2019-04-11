90e07056d14a1d10e2548d9a82668564d304081e
Mario Ferreira <mario.ferreira.neto@accenture.com>
2019-04-11 10:39:41 -0300

    Avanco nos exercicios

--- script/lista.js
+++ script/lista.js
@@ -1,5 +1,6 @@
 let atualPage = 1;
 let lastPage;
+let atualData;
 
 $(function(){
     services.lista(1, callbackLista);
@@ -27,14 +28,52 @@ $(function(){
         services.lista(lastPage, callbackLista);
     });
 
+    $('.ordenar-id').on('click', function(){
+        let _t = $(this);
+        if(_t.hasClass('asc')){
+            _t.removeClass('asc').addClass('desc');
+            montaTabela(atualData.data.sort((a,b) => {
+                if (a.id < b.id) { return 1; }
+                if (a.id > b.id) { return -1; }
+                return 0;
+            }));
+        }else{
+            _t.removeClass('desc').addClass('asc');
+            montaTabela(atualData.data.reverse((a,b) => {
+                if (a.id < b.id) { return 1; }
+                if (a.id > b.id) { return -1; }
+                return 0;
+            }));
+        }
+    });
+
+    $('.ordenar-nome').on('click', function(){
+        let _t = $(this);
+        if(_t.hasClass('asc')){
+            _t.removeClass('asc').addClass('desc');
+            montaTabela(atualData.data.sort(function(a,b){
+                return b.first_name.localeCompare(a.first_name);
+            }));
+        }else if(_t.hasClass('desc')){
+            _t.removeClass('desc').addClass('asc');
+            montaTabela(atualData.data.sort(function(a,b){
+                return a.first_name.localeCompare(b.first_name);
+            }));
+        }
+    });
+
 });
 
-const callbackLista = (data) => {
-    
-    let itens = data.data;
+const callbackLista = (obj) => {
+    atualData = obj;
+    let itens = obj.data;
+    montaTabela(itens);
+}
+
+const montaTabela = (itens) => {
     let bodyTable = '';
-    atualPage = data.page;
-    lastPage = data.total_pages;
+    atualPage = atualData.page;
+    lastPage = atualData.total_pages;
     for(let i = 0; i < itens.length; i++){
         bodyTable += '<tr>'+
                         '<th scope="row">'+itens[i].id+'</th>'+
@@ -47,21 +86,22 @@ const callbackLista = (data) => {
     $('.table-striped tbody').html('').append(bodyTable);
     $('.btn-pags').html('');
     
-    for(let i = 1; i <= data.total_pages; i++){
+    for(let i = 1; i <= lastPage; i++){
         $('.btn-pags').append('<button id="pag-'+i+'" class="btn-numbers">'+i+'</button>');
     }
     
-    $('#pag-'+data.page).attr('disabled', true);
+    $('#pag-'+atualPage).attr('disabled', true);
 
-    if(data.page == data.total_pages){
+    if(atualPage == lastPage){
         $('.button-right button').attr('disabled', true);
     }else{
         $('.button-right button').removeAttr('disabled');
     }
 
-    if(data.page == 1){
+    if(atualPage == 1){
         $('.button-left button').attr('disabled', true);
     }else{
         $('.button-left button').removeAttr('disabled');
     }
 }
+
